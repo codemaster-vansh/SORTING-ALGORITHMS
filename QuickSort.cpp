431675cfdc347ba35bc6f91ee79944ebb4fa6713
codemaster-vansh <vanshcodes124@gmail.com>
2025-04-16 21:52:59 +0530

    AddingQuickSort

---
+++ QuickSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+void quicksort(vector<int>& array, int low, int high){
+    if (low >= high){
+        return;
+    }
+    int pivot = array[high];
+
+    int left = low;
+    int right = high;
+    int temp;
+
+    while (left < right){
+        while(array[left] <= pivot && left < right){
+            left++;
+        }
+        while(array[right] > pivot && left < right){
+            right--;
+        }
+        temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+    array[high] = array[left];
+    array[left] = pivot;
+    quicksort(array, low, left - 1);
+    quicksort(array, left + 1, high);
+}
